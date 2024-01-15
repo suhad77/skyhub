42f4e8be7716f95dc7c53c955babb92d83729e3d
suhad77 <suhadmia12@gmail.com>
2024-01-15 18:31:01 +0600

    about page done

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ new VenoBox({
     selector: '.my-video-links',
 });
 AOS.init();
+
+// about page
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    loop: true,
+    autoplay: true,
+});
